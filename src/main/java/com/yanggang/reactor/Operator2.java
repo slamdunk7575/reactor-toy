6f3040595a6e917b84bcb2ab3be6b797ed74545f
slamdunk7575 <slamdunk7575@gmail.com>
2024-07-03 00:02:13 +0900

    (feat) Operator 동작 확인 (flatMapMany, defaultItEmpty, switchIfEmpty, merge, zip)

--- src/main/java/com/yanggang/reactor/Operator2.java
+++ src/main/java/com/yanggang/reactor/Operator2.java
@@ -1,8 +1,10 @@
 package com.yanggang.reactor;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
+import java.util.List;
 
 public class Operator2 {
     // concatMap : 동기적으로 처리해야 할때는 flatMap 대신 concatMap 사용
@@ -13,11 +15,53 @@ public class Operator2 {
                 .log();
     }
 
-    // flatMapMany
-
-    // switchIfMany
+    // flatMapMany : Mono To Flux
+    public Flux<Integer> monoFlatMapMany() {
+        return Mono.just(10)
+                .flatMapMany(i -> Flux.range(1, i))
+                .log();
+    }
 
     // defaultIfMany
+    public Mono<Integer> defaultItEmpty() {
+        return Mono.just(100)
+                .filter(i -> i > 100)
+                .defaultIfEmpty(10);
+    }
 
-    // merge & zip
+    // switchIfMany
+    public Mono<Integer> switchIfEmpty1() {
+        return Mono.just(100)
+                .filter(i -> i > 100)
+                .switchIfEmpty(Mono.just(30).map(i -> i * 2));
+    }
+
+    public Mono<Integer> switchIfEmpty2() {
+        return Mono.just(100)
+                .filter(i -> i > 100)
+                .switchIfEmpty(Mono.error(new RuntimeException("예외 발생")))
+                .log();
+    }
+
+    // merge
+    public Flux<String> fluxMerge() {
+        return Flux.merge(Flux.fromIterable(List.of("a", "b", "c", "d", "e")), Flux.just("g"));
+    }
+
+    public Flux<String> monoMerge() {
+        return Mono.just("1").mergeWith(Mono.just("2")).mergeWith(Mono.just("3"));
+    }
+
+    // zip: 각 인덱스에 맞춰 묶어서 연산을 할 수 있게 도와주는 연산자 (예: (a,b) (b,e) (c,f))
+    public Flux<String> fluxZip() {
+        return Flux.zip(Flux.just("a", "b", "c"), Flux.just("d", "e", "f"))
+                .map(i -> i.getT1() + i.getT2())
+                .log();
+    }
+
+    public Mono<Integer> monoZip() {
+        return Mono.zip(Mono.just(1), Mono.just(2), Mono.just(3))
+                .map(i -> i.getT1() + i.getT2() + i.getT3())
+                .log();
+    }
 }
--- src/test/java/com/yanggang/reactor/Operator2Test.java
+++ src/test/java/com/yanggang/reactor/Operator2Test.java
@@ -3,8 +3,6 @@ package com.yanggang.reactor;
 import org.junit.jupiter.api.Test;
 import reactor.test.StepVerifier;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class Operator2Test {
 
     private Operator2 operator2 = new Operator2();
@@ -16,4 +14,59 @@ class Operator2Test {
                 .verifyComplete();
     }
 
+    @Test
+    void monoFlatMapMany() {
+        StepVerifier.create(operator2.monoFlatMapMany())
+                .expectNext(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .verifyComplete();
+    }
+
+    @Test
+    void defaultItEmpty() {
+        StepVerifier.create(operator2.defaultItEmpty())
+                .expectNext(10)
+                .verifyComplete();
+    }
+
+    @Test
+    void switchIfEmpty1() {
+        StepVerifier.create(operator2.switchIfEmpty1())
+                .expectNext(60)
+                .verifyComplete();
+    }
+
+    @Test
+    void switchIfEmpty2() {
+        StepVerifier.create(operator2.switchIfEmpty2())
+                .expectError(RuntimeException.class)
+                .verify();
+    }
+
+    @Test
+    void fluxMerge() {
+        StepVerifier.create(operator2.fluxMerge())
+                .expectNext("a", "b", "c", "d", "e", "g")
+                .verifyComplete();
+    }
+
+    @Test
+    void monoMerge() {
+        StepVerifier.create(operator2.monoMerge())
+                .expectNext("1", "2", "3")
+                .verifyComplete();
+    }
+
+    @Test
+    void fluxZip() {
+        StepVerifier.create(operator2.fluxZip())
+                .expectNext("ad", "be", "cf")
+                .verifyComplete();
+    }
+
+    @Test
+    void monoZip() {
+        StepVerifier.create(operator2.monoZip())
+                .expectNext(6)
+                .verifyComplete();
+    }
 }
